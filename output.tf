6e8860a31a2b654c414f8212d06feddd250e7126
Lakshay Arora <53274714+lakshayarora476@users.noreply.github.com>
2021-01-27 15:02:47 +0530

    removed sections other than 'details' section

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
- output "public_ip_fqdn" {
-     value = azurerm_public_ip.TerraformPublicIp.fqdn
- }
-
- output "public_ip_id" {
-     value = azurerm_public_ip.TerraformPublicIp.id
- }
-
   output "public_ip_Details" {
      value = azurerm_public_ip.TerraformPublicIp
  }
