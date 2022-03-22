174573453a6af43864c2bd284d9981ff987287a8
reena nain <94439846+reena-nai@users.noreply.github.com>
2022-03-22 21:40:23 +0530

    terraform fmt checked

--- example/main.tf
+++ example/main.tf
@@ -3,10 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-        version = "=2.86.0"
-     }
-   }
- }
+      version = "=2.86.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 
 module "res_group" {
-  source                  = "git::https://github.com/OT-terraform-azure-modules/terraform-azure-resource-group.git?ref=master"
+  source                  = "OT-terraform-azure-modules/resource-group/azure"
   resource_group_name     = "azure-rg"
   resource_group_location = "West Europe"
   lock_level_value        = ""
@@ -25,13 +25,13 @@ module "res_group" {
 
 
 module "public-ip_module" {
-  source              = "git::https://github.com/OT-terraform-azure-modules/terraform-azure-public-ip.git?ref=v0.0.1" 
+  source              = "git::https://github.com/OT-terraform-azure-modules/terraform-azure-public-ip.git?ref=v0.0.1"
   resource_group_name = module.res_group.resource_group_name
   location            = module.res_group.resource_group_location
   allocation_method   = "Dynamic"
-  pub_ip_name         = ["pub1" ,"pub2"]
+  pub_ip_name         = ["pub1", "pub2"]
   sku                 = "Basic"
-  tags                = {
+  tags = {
     env : ""
     author : ""
   }
