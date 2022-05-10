959ca78d0a829d4ae8ad4ac32aa968f6dfd5a784
reena nain <94439846+reena-nai@users.noreply.github.com>
2022-05-10 19:29:40 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "public_ip_id" {
   value       = azurerm_public_ip.public_ip.*.id
   description = "The id of public-ip get from this"
 }
+
+output "fqdn"{
+  value = azurerm_public_ip.public_ip.fqdn
+}
