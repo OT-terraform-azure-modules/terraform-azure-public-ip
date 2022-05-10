1299f59a8a192a1b53c0ebdf5bb7699b11f2e265
reena nain <94439846+reena-nai@users.noreply.github.com>
2022-05-10 19:38:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,7 +26,6 @@ variable "sku" {
 }
 
 variable "domain_name_label" {
-  type        = string
   description = "(Optional) Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
   default     = ""
 }
