6b4667acdd8f819c1a8d3ebaf71d726a68e5f351
reena nain <94439846+reena-nai@users.noreply.github.com>
2022-05-10 19:53:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_public_ip" "public_ip" {
-    count               = length(var.public_ip_name)
-    name                = var.public_ip_name[count.index]
+   # count               = length(var.public_ip_name)
+    #name                = var.public_ip_name[count.index]
+    name                 = var.public_ip_name
     location            = var.location
     resource_group_name = var.resource_group_name
     domain_name_label   = var.domain_name_label
