c850162fe8d3d6f8b833beccfa51acc18419db5a
kulsinghdeep <93075914+kulsinghdeep@users.noreply.github.com>
2024-07-26 21:08:42 +0530

    Update output.tf for public ip address

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "public_ip_id" {
   value       = azurerm_public_ip.public_ip.*.id
   description = "The id of public-ip get from this"
 }
+
+output "public_ip_address" {
+  value       = azurerm_public_ip.public_ip.*.ip_address 
+  description = "The ip address of public-ip get from this"
+}
